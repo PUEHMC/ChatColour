95f784191438aae7b8c277d484e0ecc2b4c493bd
FandMC <winme2002@outlook.com>
2025-09-06 22:06:14 +0800

    升级1.21

--- src/main/java/cn/puehmc/ChatListener.java
+++ src/main/java/cn/puehmc/ChatListener.java
@@ -1,9 +1,12 @@
 package cn.puehmc;
 
+import io.papermc.paper.event.player.AsyncChatEvent;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 /**
  * 聊天监听器类
@@ -20,19 +23,29 @@ public class ChatListener implements Listener {
     }
     
     /**
-     * 处理玩家聊天事件
+     * 处理玩家聊天事件 (Paper API)
      * @param event 聊天事件
      */
-    @EventHandler
-    public void onPlayerChat(AsyncPlayerChatEvent event) {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerChat(AsyncChatEvent event) {
         Player player = event.getPlayer();
-        String message = event.getMessage();
+        
+        // 将Adventure Component转换为字符串
+        String originalMessage = LegacyComponentSerializer.legacySection().serialize(event.message());
         
         // 检查玩家是否有权限使用颜色代码
         if (permissionChecker.canUseColors(player)) {
             // 转换颜色代码
-            String coloredMessage = colorProcessor.translateColorCodes(message);
-            event.setMessage(coloredMessage);
+            String coloredMessage = colorProcessor.translateColorCodes(originalMessage);
+            
+            // 将处理后的字符串转换回Adventure Component
+            Component newMessage = LegacyComponentSerializer.legacySection().deserialize(coloredMessage);
+            event.message(newMessage);
+            
+            // 调试信息 - 输出到控制台
+            System.out.println("[ChatColour] 玩家: " + player.getName() + ", 原始消息: " + originalMessage + ", 处理后: " + coloredMessage);
+        } else {
+            System.out.println("[ChatColour] 玩家 " + player.getName() + " 没有颜色代码权限");
         }
     }
 }
